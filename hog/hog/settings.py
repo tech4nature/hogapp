67a592a86dbb0da2083d089b8aebdbb32d2d7318
Seb Bacon <seb.bacon@gmail.com>
2019-04-02 19:54:40 +0100

    Add to allowed hosts

--- hog/hog/settings.py
+++ hog/hog/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hogapp', 'localhost']
+ALLOWED_HOSTS = ['hogapp', 'localhost', 'connectionengine.co.uk']
 
 
 # Application definition
